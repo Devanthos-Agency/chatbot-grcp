ebebeec7a1346bf1219bb152743615456b85f5b1
Julian Peruzzi <peru964@gmail.com>
2025-10-08 05:57:58 -0300

    Update message.tsx

--- src/components/ai-elements/message.tsx
+++ src/components/ai-elements/message.tsx
@@ -73,7 +73,7 @@ export const MessageAvatar = ({
   className,
   ...props
 }: MessageAvatarProps) => (
-  <Avatar className={cn("size-16 ring-1 ring-border", className)} {...props}>
+  <Avatar className={cn("size-12 ring-1 ring-border", className)} {...props}>
     <AvatarImage alt="" className="mt-0 mb-0" src={src} />
     <AvatarFallback>{name?.slice(0, 2) || "ME"}</AvatarFallback>
   </Avatar>
